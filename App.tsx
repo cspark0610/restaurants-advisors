f681d70753c6c7d855c8d37441074fcd93ba874e
Carlos S. Park <75094281+cspark0610@users.noreply.github.com>
2023-03-26 11:17:27 -0300

    fix: change in babel.,config, loads fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LogBox } from 'react-native';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+//import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { AppNavigation } from './src/navigation/AppNavigation';
@@ -10,9 +10,22 @@ import { firebaseConfig } from './src/utils/firebase.config';
 import { initializeApp } from 'firebase/app';
 LogBox.ignoreAllLogs();
 
+import * as Font from 'expo-font';
+import * as Icon from '@expo/vector-icons';
+
+export const _loadResourcesAsync = async () => {
+  return Promise.all([
+    Font.loadAsync({
+      ...Icon.Ionicons.font,
+      ...Icon.MaterialCommunityIcons.font,
+    }),
+  ]);
+};
+
 // se debe inicializar la app de firebase desde el componete ppal de la app
 initializeApp(firebaseConfig);
 function App() {
+  _loadResourcesAsync();
   return (
     <>
       <NavigationContainer>
--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,28 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        envName: 'APP_ENV',
-        moduleName: '@env',
-        path: '.env',
-        blocklist: null,
-        allowlist: [
-          'FIREBASE_API_KEY',
-          'FIREBASE_AUTH_DOMAIN',
-          'FIREBASE_PROJECT_ID',
-          'FIREBASE_STORAGE_BUCKET',
-          'FIREBASE_MESSAGING_SENDER_ID',
-          'FIREBASE_APP_ID',
-        ],
-        safe: false,
-        allowUndefined: false,
-        verbose: false,
-      },
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          envName: 'APP_ENV',
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: [
+            'FIREBASE_API_KEY',
+            'FIREBASE_AUTH_DOMAIN',
+            'FIREBASE_PROJECT_ID',
+            'FIREBASE_STORAGE_BUCKET',
+            'FIREBASE_MESSAGING_SENDER_ID',
+            'FIREBASE_APP_ID',
+          ],
+          safe: false,
+          allowUndefined: false,
+          verbose: false,
+        },
+      ],
     ],
-  ],
+  };
 };
